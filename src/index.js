7aaff297fee4d775d49585d46c4979941c36a5e8
Benny Ogidan <benny.ogidan@chemondis.com>
2021-11-20 10:56:02 +0100

    chore(setup): Initialiaze Repo 🎉  - Setup prettier  - Setup eslint, babel  - Setup styled-components

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import ReactDOM from "react-dom"
+
+
+
+
+const App = () => {
+  return (
+  <Wrapper onClick={handleClick}>
+    test
+  </Wrapper>
+)}
+ReactDOM.render(<App />, document.getElementById('app'))
